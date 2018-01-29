9c9e0d845f639d6cb3aa90fc791fa1a34fa2ac6e
Administrator <Administrator@DEEP-1611251137>
2018-01-29 18:04:35 +0800

    基础代码

---
+++ CYY/CYY.Base/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CYY.Base
+{
+    public class Class1
+    {
+        //Console.Write();
+    }
+}
